ca248a79e2032a0d99817e534694ab30bf181823
Arina Zubova <arina777pototskaya@gmail.com>
2022-05-11 17:46:02 +0300

    update prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,17 +1,16 @@
 from random import randint
+from math import sqrt
 
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def task_and_right_answer():
-    num = randint(1, 100)
+    num = randint(2, 100)
     right_answer = is_prime(num)
     task = num
     return (task, right_answer)
 
 
 def is_prime(num):
-    for i in range(2, num // 2 + 1):
-        if num % i == 0:
-            return 'no'
-    return 'yes'
+    for i in range(2, int(sqrt(num)) + 1):
+        return 'no' if num % i == 0 else 'yes'
