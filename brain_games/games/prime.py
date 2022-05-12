38c79751cb8d0177c8eedfd658cdbb0fd10ce6d4
Arina Zubova <arina777pototskaya@gmail.com>
2022-05-12 11:19:40 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,7 +5,6 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def task_and_right_answer():
-    num = randint(1, 100)
-    right_answer = 'yes' if sympy.isprime(num) == True else 'no'
-    task = num
+    task = randint(1, 100)
+    right_answer = 'yes' if sympy.isprime(task) else 'no'
     return (task, right_answer)
